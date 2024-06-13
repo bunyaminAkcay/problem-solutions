5668e0474305b74a4d3ef5aac1e4251adbf2c125
bunyaminAkcay <bunyamin.akcay@outlook.com>
2024-06-14 01:09:27 +0300

    format spital

--- c/spiral.c
+++ c/spiral.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 #define N 20
 
-void rotateDirection(int* direction){
+void rotateDirection(int *direction)
+{
     int sign = 1;
-    if (direction[1] != 0){
+    if (direction[1] != 0)
+    {
         sign = -1;
     }
     int temp = direction[1];
@@ -11,10 +13,11 @@ void rotateDirection(int* direction){
     direction[0] = temp * sign;
 }
 
-int main() {
+int main()
+{
 
     int direction[2] = {1, 0};
-    int location[2] = {(N-1)/2, (N-1)/2};
+    int location[2] = {(N - 1) / 2, (N - 1) / 2};
     int area[N][N] = {{0}};
 
     int l = 1;
@@ -24,9 +27,10 @@ int main() {
 
     area[location[0]][location[1]] = 1;
 
-    while (index < N*N){
-
-        for(int t=0; t < l ;t++){
+    while (index < N * N)
+    {
+        for (int t = 0; t < l; t++)
+        {
             location[0] += direction[0];
             location[1] += direction[1];
 
@@ -41,11 +45,14 @@ int main() {
             l++;
     }
 
-    for (int i=0; i < N; i++){
-        for (int j=0; j < N; j++){
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < N; j++)
+        {
             printf("%d\t", area[j][i]);
         }
         printf("\n");
     }
+
     return 0;
 }
